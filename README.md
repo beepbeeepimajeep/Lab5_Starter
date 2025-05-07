947bbc45071fd899276a19953e68f1885390024a
beepbeeepimajeep <dvaneetv@ucsd.com>
2025-05-06 17:43:41 -0700

    added DOM element refs in expose.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab 5 - Starter
-Name: Rain van Eetveldt
+Names: Rain van Eetveldt, Luis Zaragoza
+
+## Rain's notes:
+- No comments allowed in package.json
+- The package.json file is: Part of the Node.js development setup which is used to define: Project metadata (name, version), Scripts (like npm test) and dependencies (like Jest for testing).
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,16 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //DOM element references
+  /* even though const type we can still use or modify the element 
+  it points to */
+  const hornSelect = document.getElementById('horn-select');
+  //select the first <img> element inside the #expose element in .css
+  const hornImage = document.querySelector('#expose img');
+  //finds the first audio tag in expose.html
+  const hornAudio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+  const confetti = new JSConfetti();
 }
