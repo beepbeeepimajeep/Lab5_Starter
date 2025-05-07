f46df5f18b6c1ff2b8d14fafea877f138ee3ac1e
Luis Zaragoza <luisenriquezaragoza@gmail.com>
2025-05-06 19:51:21 -0700

    Completed Expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -23,5 +23,37 @@ function init() {
     hornImage.alt = `${selected} image`;
     hornAudio.src = `assets/audio/${selected}.mp3`;
   });
+  
+  //When button is clocked play horn and if it is the party horn add confetti
+  playButton.addEventListener('click', () => {
+    hornAudio.play();
 
+    if (hornSelect.value === 'party-horn') {
+      confetti.addConfetti();
+    }
+  });
+  
+
+  //When the volume slider is being dragged change the volume
+  volumeSlider.addEventListener('input', () => {
+    hornAudio.volume = volumeSlider.value / 100;
+    
+    //Get volume level
+    const volume = volumeSlider.value; 
+    // Update the icon based on the volume
+    if (volume == 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      volumeIcon.alt = 'Volume level 0';
+    } else if (volume >= 1 && volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      volumeIcon.alt = 'Volume level 1';
+    } else if (volume >= 33 && volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      volumeIcon.alt = 'Volume level 2';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.alt = 'Volume level 3';
+    }
+  });
+  
 }
