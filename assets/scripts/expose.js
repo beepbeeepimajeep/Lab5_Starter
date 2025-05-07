3d76c5534b9229a748a969a4627b1cf2241c66ad
beepbeeepimajeep <dvaneetv@ucsd.com>
2025-05-06 17:51:21 -0700

    added volume control in expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -23,5 +23,35 @@ function init() {
     hornImage.alt = `${selected} image`;
     hornAudio.src = `assets/audio/${selected}.mp3`;
   });
-
+  
+  //volume and  icon
+  volumeSlider.addEventListener('input', () => {
+    const volume = parseInt(volumeSlider.value);
+    //convert from sliders 0-100 range to <audio> 0.0-1.0 range
+    hornAudio.volume = volume / 100;
+    if (volume === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      volumeIcon.alt = 'Volume level 0';
+    } 
+    else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      volumeIcon.alt = 'Volume level 1';
+    } 
+    else if (volume < 67) {
+       volumeIcon.src = 'assets/icons/volume-level-2.svg';
+       volumeIcon.alt = 'Volume level 2';
+    } 
+    else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.alt = 'Volume level 3';
+    }
+  });
+ 
+  //horn sound and confetti if party horn
+  playButton.addEventListener('click', () => {
+    hornAudio.play();
+    if (hornSelect.value === 'party-horn') {
+       confetti.addConfetti();
+    }
+  });
 }
